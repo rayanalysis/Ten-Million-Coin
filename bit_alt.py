0bcc493e2f7c6fb971652b95567f921b3f7b8c81
rayanalysis <ray.analysis@gmail.com>
2020-10-25 17:52:22 -0600

    Update bit_alt.py

--- bit_alt.py
+++ bit_alt.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2020 Logan Bier
-# 
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -9,19 +9,19 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+
 # project details
 # a time-bound distributed currency
 # that replaces chain exponential energy usage
 # with a finite energy distributed ledger
 # made up of a randomly generated set of values
 # in a range of natural numbers
-#
+
 # conceptually, energy is conserved by awarding the
 # winning guess the Nth "coin" in a stepwise registry
 # of the vector of secret randomly generated values
 # with the Unix Time as a standard of linear computation
-#
+
 # given a finite cycle time to guess the registry value,
 # the winning computer is the first one which guesses 
 # correctly in a normalized time range across wallets
